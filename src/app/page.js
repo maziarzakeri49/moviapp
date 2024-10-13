f95c428217a29403f3a329669cc7d54e36c7ebb1
maziarzakeri49 <maziarzakeri49@gmail.com>
2024-10-13 14:12:47 -0400

    fetch data from TMDB api to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
-import React from 'react'
 
-export default function Home() {
+import Results from '@/components/Results';
+import React from 'react'
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({searchParams}) {
+  const genre=searchParams.genre || 'fetchTrending';
+  
+  const res=await fetch(`https://api.themoviedb.org/3${genre==='fetchTopRated' ?'/movie/top_rated' : '/trending/all/week'}?api_key=${API_KEY}&language=en-US&page=1`);
+  const data = await res.json();
+  if(!res.ok){
+    throw new Error("this fetch error");
+    
+  }
+  const results=data.results
+ 
   return (
     <div>
-      Home
+      <Results results={results}/>
+     
     </div>
   )
 }
