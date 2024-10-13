b5fd88b5c6f9b77551265bf591b3f64e3a404f74
maziarzakeri49 <maziarzakeri49@gmail.com>
2024-10-13 10:22:31 -0400

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='max-w-6xl mx-auto space-y-8 p-3'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis repellat sed animi optio obcaecati? Id sed vel, mollitia, veritatis nisi dicta consequuntur voluptas quod eveniet labore, fugit placeat omnis. Sapiente.Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis repellat sed animi optio obcaecati? Id sed vel, mollitia, veritatis nisi dicta consequuntur voluptas quod eveniet labore, fugit placeat omnis. Sapiente.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis repellat sed animi optio obcaecati? Id sed vel, mollitia, veritatis nisi dicta consequuntur voluptas quod eveniet labore, fugit placeat omnis. Sapiente.Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis repellat sed animi optio obcaecati? Id sed vel, mollitia, veritatis nisi dicta consequuntur voluptas quod eveniet labore, fugit placeat omnis. Sapiente.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis repellat sed animi optio obcaecati? Id sed vel, mollitia, veritatis nisi dicta consequuntur voluptas quod eveniet labore, fugit placeat omnis. Sapiente.Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis repellat sed animi optio obcaecati? Id sed vel, mollitia, veritatis nisi dicta consequuntur voluptas quod eveniet labore, fugit placeat omnis. Sapiente.</p>
+      
+    </div>
+  )
+}
+
+export default About
