d293c6494653b1d01374008b6270e4d35712c787
maziarzakeri49 <maziarzakeri49@gmail.com>
2024-10-13 15:49:22 -0400

    create loading page

---
+++ src/app/loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className='flex justify-center'>
+        <img src="spinner.svg" alt="Loading..." />
+      
+    </div>
+  )
+}
